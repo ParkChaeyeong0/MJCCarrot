bf68db63f239bf4d00680734330d679b22009065
So-Yeong1 <56152775+So-Yeong1@users.noreply.github.com>
2020-10-15 02:18:07 +0900

    Coin
    Destroy로 코인이랑 충돌했을 시 코인 삭제

---
+++ Assets/02.Scripts/Coin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider c)
+    { 
+        //Is Trigger 체크하기
+        if(c.gameObject.name == "Coin")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
