37c18e41f76855e814a4396f5ee93a2ab9084008
ParkChaeyeong0 <0726tkfkdgo@naver.com>
2020-10-18 20:29:50 +0900

    메인화면 기능추가
    설정 창 띄우기, Start버튼, Skins버튼 누르면 씬 이동, Skins 화면 배치, 배경 에셋 추가

---
+++ Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject settingImg;
+
+    public void OnClick_Start()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void OnClick_Skins()
+    {
+        SceneManager.LoadScene("SkinScene");
+    }
+
+    public void OnClick_SkinsClose()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnClick_SettingButton()
+    {
+        settingImg.SetActive(true);
+    }
+
+    public void OnClick_SettingButtonClose()
+    {
+        settingImg.SetActive(false);
+    }
+}
