8c3c2ef50c81777f69ee7ef07b2e258746bd80e6
ParkChaeyeong0 <0726tkfkdgo@naver.com>
2020-10-22 17:32:04 +0900

    화면 비율 고정 시도
    메인화면, 스킨 화면 빌드 화면에 맞추는 것, 플레이 화면 구도 바꿈

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,6 +8,44 @@ public class GameManager : MonoBehaviour
 {
     public GameObject settingImg;
 
+    public void setupCamera()
+    {
+        //가로 화면 비율
+        float targetWidthAspect = 16.0f;
+
+        //세로 화면 비율
+        float targetHeightAspect = 10.0f;
+
+        //메인 카메라
+        Camera mainCamera = Camera.main;
+
+        mainCamera.aspect = targetWidthAspect / targetHeightAspect;
+
+        float widthRatio = (float)Screen.width / targetWidthAspect;
+        float heightRatio = (float)Screen.height / targetHeightAspect;
+
+        float heightadd = ((widthRatio / (heightRatio / 100)) - 100) / 200;
+        float widthtadd = ((heightRatio / (widthRatio / 100)) - 100) / 200;
+
+        // 16_10비율보다 가로가 짧다면 (4_3비율)
+        // 16_10비율보다 세로가 짧다면 (16_9비율)
+        // 시작 지점을 0으로 만들어준다.
+
+        if (heightRatio > widthRatio)
+        {
+            widthtadd = 0.0f;
+        }
+        else
+            heightadd = 0.0f;
+
+        mainCamera.rect = new Rect(
+            mainCamera.rect.x + Math.Abs(widthtadd),
+            mainCamera.rect.y + Math.Abs(heightadd),
+            mainCamera.rect.width + (widthtadd * 2),
+            mainCamera.rect.height + (heightadd * 2));
+    }
+
+    // 씬 이동
     public void OnClick_Start()
     {
         SceneManager.LoadScene("PlayScene");
@@ -22,6 +61,7 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene("MainScene");
     }
 
+    // 설정 창 띄우고 닫는 버튼
     public void OnClick_SettingButton()
     {
         settingImg.SetActive(true);
