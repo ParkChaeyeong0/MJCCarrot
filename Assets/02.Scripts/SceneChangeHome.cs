dc27eb0264f2b9be8d5b8c13e857791252a7f13f
So-Yeong1 <56152775+So-Yeong1@users.noreply.github.com>
2020-10-23 21:18:08 +0900

    Home버튼
    Game Over 창에서 home 버튼 누를 시 MainScene으로 이동

---
+++ Assets/02.Scripts/SceneChangeHome.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeHome : MonoBehaviour
+{
+    public void ChangePlayScene()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void ChangeMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
