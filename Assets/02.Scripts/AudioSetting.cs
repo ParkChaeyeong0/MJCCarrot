6454ccd307f8af63076492fb9dfd4d4f7a3c31bd
ParkChaeyeong0 <0726tkfkdgo@naver.com>
2020-11-19 07:49:33 +0900

    Sound 구현 중...
    버튼 UI 변경 및 사운드 구현 중

---
+++ Assets/02.Scripts/AudioSetting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSetting : MonoBehaviour
+{
+    //public AudioMixer masterMixer;
+    public Slider audioSlider;
+    public AudioSource audio;
+
+    private float backVol = 1f;
+
+    private void Start()
+    {
+        backVol = PlayerPrefs.GetFloat("backvol", 1f);
+        audioSlider.value = backVol;
+        audio.volume = audioSlider.value;
+    }
+
+    void Update()
+    {
+        SoundSlider();
+    }
+
+    public void SoundSlider()
+    {
+        audio.volume = audioSlider.value;
+
+        backVol = audioSlider.value;
+        PlayerPrefs.SetFloat("backvol", backVol);
+    }
+}
