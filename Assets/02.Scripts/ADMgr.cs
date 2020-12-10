9e2f35f5895fa1f35c9628f7e85859fc9a1d1efb
So-Yeong1 <56152775+So-Yeong1@users.noreply.github.com>
2020-12-11 05:26:33 +0900

    광고

---
+++ Assets/02.Scripts/ADMgr.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ADMgr : MonoBehaviour
+{
+    public void ShowAD()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+    }
+}
