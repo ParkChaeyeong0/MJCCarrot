3a221df2e2c9727153777b2a287412813dd7816e
ParkChaeyeong0 <0726tkfkdgo@naver.com>
2020-12-11 16:39:18 +0900

    소리 조정 중
    - 플레이화면 소리 조정 중

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -147,46 +147,4 @@ public class GameManager : MonoBehaviour
     {
         producerImg.SetActive(false);
     }
-
-    // 화면 비율 고정
-    public void setupCamera()
-    {
-        //가로 화면 비율
-        float targetWidthAspect = 16.0f;
-
-        //세로 화면 비율
-        float targetHeightAspect = 10.0f;
-
-        //메인 카메라
-        Camera mainCamera = Camera.main;
-
-        mainCamera.aspect = targetWidthAspect / targetHeightAspect;
-
-        float widthRatio = (float)Screen.width / targetWidthAspect;
-        float heightRatio = (float)Screen.height / targetHeightAspect;
-
-        float heightadd = ((widthRatio / (heightRatio / 100)) - 100) / 200;
-        float widthtadd = ((heightRatio / (widthRatio / 100)) - 100) / 200;
-
-        // 16_10비율보다 가로가 짧다면 (4_3비율)
-        // 16_10비율보다 세로가 짧다면 (16_9비율)
-        // 시작 지점을 0으로 만들어준다.
-
-        if (heightRatio > widthRatio)
-        {
-            widthtadd = 0.0f;
-        }
-        else
-            heightadd = 0.0f;
-
-        mainCamera.rect = new Rect(
-            mainCamera.rect.x + Math.Abs(widthtadd),
-            mainCamera.rect.y + Math.Abs(heightadd),
-            mainCamera.rect.width + (widthtadd * 2),
-            mainCamera.rect.height + (heightadd * 2));
-    }
-}
-
-internal class GameScore
-{
 }
---
+++ Assets/02.Scripts/PlaySounManger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySounManger : MonoBehaviour
+{
+    GameOver gameOver;
+
+    
+    public GameObject BGMStop;
+    public GameObject GameOverSound;
+
+    void Start()
+    {
+        gameOver = GameObject.Find("Player").GetComponent<GameOver>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameOver.isEnter);
+
+        if (gameOver.isEnter)
+        {
+            AudioSource BGMSound = BGMStop.GetComponent<AudioSource>();
+            AudioSource GameOver = GameOverSound.GetComponent<AudioSource>();
+
+            GameOver.Play();
+            BGMSound.Stop();
+        }
+    }
+}
