a5663faef3aa0e3a4ecc8efdb4a4fdfe0df688c2
Hyunki Kim <khk6916@naver.com>
2020-12-15 18:40:51 +0900

    스킬쿨적용

--- Assets/02.Scripts/UseItem.cs
+++ Assets/02.Scripts/UseItem.cs
@@ -1,21 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UseItem : MonoBehaviour
 {
 
     MapMove []mapMove ;
     public GameObject playerChracter;
-
+    public Image speed, shield, magnet;
     
     void Start()
     {
         mapMove = GameObject.FindObjectsOfType<MapMove>();
         playerChracter = GameObject.FindWithTag("Player");
+        
    
     }
-
 
     public void UseSpeedDown()
     {
@@ -23,12 +23,52 @@ public class UseItem : MonoBehaviour
         {
             mapMove[i].moveSpeed = 8;
         }
+
+        speed.gameObject.SetActive(true);
+
+        StartCoroutine(SpeedDownCooltime());
         
     }
 
     public void UseShield()
     {
         StartCoroutine("Invisivle");
+
+        shield.gameObject.SetActive(true);
+
+        StartCoroutine(ShieldCooltime());
+    }
+
+
+
+    public void UseMagnet()
+    {
+        magnet.gameObject.SetActive(true);
+
+        StartCoroutine(MagnetCooltime());
+    }
+
+    
+    IEnumerator SpeedDownCooltime()
+    {
+        yield return new WaitForSeconds(60f);
+        speed.gameObject.SetActive(false);
+      
+    }
+
+
+    IEnumerator ShieldCooltime()
+    {
+        yield return new WaitForSeconds(30f);
+        shield.gameObject.SetActive(false);
+
+    }
+
+    IEnumerator MagnetCooltime()
+    {
+        yield return new WaitForSeconds(30f);
+        magnet.gameObject.SetActive(false);
+
     }
 
     IEnumerator Invisivle()
@@ -36,14 +76,6 @@ public class UseItem : MonoBehaviour
         playerChracter.layer = 10;
         yield return new WaitForSeconds(5.0f);
         playerChracter.layer = 0;
-        
-    }
 
-    public void UseMagnet()
-    {
-
-    }
-
-    
-
+    }// 무적시간
 }
